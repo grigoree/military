406c5ac87c39b2d2b243c5caf6a27638c52e1e43
grigoree <grigoree@icloud.com>
2016-04-03 21:32:14 +0300

    Добваил функцию X

--- military.cpp
+++ military.cpp
@@ -1,32 +1,54 @@
 #include <iostream>
 #include <sstream>
 #include <bitset>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-int main() {
+// X
+
+bitset<4>* funcX(char a[33], char k[33]) {
     
-    int n;
-    
-    const char a[33] = "1122334455667700ffeeddccbbaa9988";
-    const char k[33] = "8899aabbccddeeff0011223344556677";
-    
-    istringstream("2A") >> hex >> n;
-    cout << std::bin << "Parsing \"2A\" as hex gives " << n << '\n';
+    bitset<4>* result = new bitset<4>[32];
     
     for (int i = 0; i < 32; i++) {
         
-         bitset<4> a_temp;
-         bitset<4> k_temp;
+        int a_temp, k_temp;
+        
+        string a_string, k_string;
+        
+        a_string += a[i];
+        k_string += k[i];
+        
+        bitset<4> a_bin;
+        bitset<4> k_bin;
+        
+        // функция переводит строку в поток с атрибутом hex 
+        istringstream(a_string) >> hex >> a_temp;
+        istringstream(k_string) >> hex >> k_temp;
          
-         istringstream(a[i]) >> std::hex >> a_temp;
-         istringstream(k[i]) >> std::hex >> k_temp;
+        a_bin = a_temp;
+        k_bin = k_temp;
+
+        bitset<4> temp = a_bin ^ k_bin;
+        
+        // отладочная печать: без to_ulong() распечатает в двоичном виде
+        cout << hex << temp.to_ulong();
          
-         cout << a[i] << " " << k[i]<< endl;
-         cout << a_temp << " " << k_temp << endl;
-         bitset<4> res = a_temp ^ k_temp;
-         cout << res << endl;
+        result[i] = temp;
     }
+    
+    return result;
+}
+
+int main() {
+
+    char a[33] = "1122334455667700ffeeddccbbaa9988";
+    char k[33] = "8899aabbccddeeff0011223344556677";
+    
+    funcX(a, k);
+    cout << endl;
     
     return 0;
 }
